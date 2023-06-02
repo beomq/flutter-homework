2b9228fef514f6e56c6508a433df0f80084b6a40
beomq <beomdolq@gmail.com>
2023-06-02 10:28:00 +0900

    ADD : 6월1일 과제 수정

--- bin/exception.dart
+++ bin/exception.dart
@@ -1,26 +1,12 @@
 void main() {
   final numString = '10';
 
-  numw(numString);
-  // int num;
-  //
-  // try {
-  //   num = int.parse(numString);
-  // } catch (e) {
-  //   num = 0;
-  // }
-  // print(num);
-}
-
-void numw(String numString) {
   int num;
 
   try {
     num = int.parse(numString);
-    print('문제없음');
   } catch (e) {
     num = 0;
-    print('문제');
   }
   print(num);
 }

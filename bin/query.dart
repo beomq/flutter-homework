65e9a59129d1d6947393f7a0e92961f94f3b5ba1
beomq <beomdolq@gmail.com>
2023-06-07 11:05:41 +0900

    ADD : 6월2일 과제 수정

--- bin/query.dart
+++ bin/query.dart
@@ -28,10 +28,24 @@ void main() {
   // 1번 문제
   final transactionSorter = transactions.where((e) => e.year == 2011).toList();
   transactionSorter.sort((a, b) => a.value.compareTo(b.value));
-
   final valueTraderSort = transactionSorter.map((e) => e.trader.name).toList();
 
+  final transactionSorter1 = transactions.where((e) => e.year == 2011).toList()
+    ..sort((a, b) => a.value.compareTo(b.value))
+    ..map((e) => e.trader.name).toList();
+
+  // cascade 연산자는 transactions 리스트에 대해 sort() 메서드를 호출한 후 반환값이
+  // 아니라 transactions 자체를 반환하기 때문에, transactionSorter1은 정렬되기 전의 리스트를
+  // 그대로 가지고 있다. 따라서 아래의 두 값은 다르다.
+  print(transactionSorter1);
   print(valueTraderSort);
+
+  final transactionSorter2 = transactions.where((e) => e.year == 2011).toList()
+    ..sort((a, b) => a.value.compareTo(b.value));
+  final valueTraderSort2 =
+      transactionSorter2.map((e) => e.trader.name).toList();
+
+  print(valueTraderSort2);
 
   // 2번 문제
   final cities = transactions.map((e) => e.trader.city).toSet().toList();
@@ -50,9 +64,15 @@ void main() {
   // 4번 문제
   final traders = transactions.map((e) => e.trader.name).toSet().toList();
 
+  // sort 메서드는 리스트를 직접 정렬 하는게 아니라 리스트를 정렬하고 null을 반환하는데,
+  // 이는 리스트를 변경하는 것이 아니라 정렬된 새로운 리스트를 반환한다. 따라서 별도의 줄에 사용해야함
   traders.sort();
 
+  final traders1 = transactions.map((e) => e.trader.name).toSet().toList()
+    ..sort();
+  // cascade연산자(..) 을 사용하면 연속된 연산이 가능하다. 코드를 간결하게 만들어줌
   print(traders);
+  print(traders1);
 
   // 5번문제
   final isMilan = transactions.any((e) => e.trader.city == 'Milan');
